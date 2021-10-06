2e3d77220fb00288ea427f2f40d710dc3a5e0291
fall9249 <eduardofaller@hotmail.com>
2021-10-05 21:46:46 -0300

    okay

---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'nova-transferencia',
+  template: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss']
+})
+
+export class NovaTansferenciaComponent{
+
+
+};
