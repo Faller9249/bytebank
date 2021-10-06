ff402f1a7a1b33062529cc62d1968351275b73be
Eduardo Faller <eduardofaller@hotmail.com>
2021-10-06 17:24:07 -0300

    Atulizaçoes de angular aula 3.04

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.index.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'bytebank';
+  transferencias: any[] = [];
+
+  transferir($event: any) {
+    console.log($event);
+    this.transferencias.push($event);
+  }
 }
